a8dd0752afbdb7029eba855574a320c5a7a16348
swadharai <swadha@live.unc.edu>
2020-08-23 17:07:10 -0400

    temporarily removed logo

--- src/components/icons/logo.js
+++ src/components/icons/logo.js
@@ -3,16 +3,21 @@ import React from 'react';
 const IconLogo = () => (
   <svg id="logo" xmlns="http://www.w3.org/2000/svg" role="img" viewBox="0 0 84 96">
     <title>Logo</title>
+    <defs>
+      <pattern id="imgpattern" x="0" y="0" width="1" height="1">
+        <image href="@images/favicons/favicon-32x32.png" x="0" y="0" width="50" height="50" />
+      </pattern>
+    </defs>
     <g transform="translate(-8.000000, -2.000000)">
       <g transform="translate(11.000000, 5.000000)">
-        <path d="" fill="currentColor" />
         <polygon
           id="Shape"
-          stroke="currentColor"
           strokeWidth="5"
+          stroke=""
           strokeLinecap="round"
           strokeLinejoin="round"
           points="39 0 0 22 0 67 39 90 78 68 78 23"
+          fill=""
         />
       </g>
     </g>
