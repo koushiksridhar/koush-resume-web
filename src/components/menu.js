a4bf6b36c9fa554bbaf73b3a2c7950be41c0385e
swadharai <swadha@live.unc.edu>
2020-08-24 12:54:47 -0400

    switched resume

--- src/components/menu.js
+++ src/components/menu.js
@@ -107,7 +107,10 @@ const Menu = ({ menuOpen, toggleMenu }) => {
                 </NavListItem>
               ))}
           </NavList>
-          <ResumeLink href="/resume.pdf" target="_blank" rel="nofollow noopener noreferrer">
+          <ResumeLink
+            href="/Rai_Swadha_2021.pdf"
+            target="_blank"
+            rel="nofollow noopener noreferrer">
             Resume
           </ResumeLink>
         </NavLinks>
